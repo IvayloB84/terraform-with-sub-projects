1e327b6120816dc9a95ba1b6ef3debb6b4b8aab4
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-03-30 17:26:06 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,13 @@
 variable "description" {
   description = "Description of the lambda."
   type        = string
+  default = "Descriptions"
 }
 
 variable "function_name" {
   description = "The function ARN of the Lambda function for which you want to create an alias."
   type        = string
   default     = "tf-lambda-git"
-}
-
-variable "function_version" {
-  description = "Lambda function version for which you are creating the alias. Pattern: ($LATEST|[0-9]+)."
-  type        = string
-  default     = ""
 }
 
 variable "compatible_runtimes" {
