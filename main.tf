949b8c262b6931e0a80c17a7a97da485b9c4d3d8
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-03-30 10:23:17 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,71 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "tf-lambda-for-module-role-9olf1xkl"
 
-module "providers" {
-  source = "./modules/providers"
+  assume_role_policy = <<EOF
+
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
 }
-  
-module "lambda" {
-  source = "./modules/lambda"
+EOF
 }
+
+resource "aws_iam_policy" "AWSLambdaBasicExecutionRole" {
+
+  name        = "AWSLambdaBasicExecutionRole-16cb508c-e9d5-4e7d-b80a-d2738e03667e"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.AWSLambdaBasicExecutionRole-16cb508c-e9d5-4e7d-b80a-d2738e03667e.arn
+}
+
+resource "null_resource" "lambda_dependencies" {
+  provisioner "local-exec" {
+    command = "cd ./lambda && npm install --legacy-peer-deps"
+  }
+}
+
+data "archive_file" "task_payload_zip" {
+  type        = "zip"
+  source_dir  = "./lambda"
+  output_path = "./task_payload.zip"
+}
+
+resource "aws_lambda_function" "task_payload" {
+  function_name    = var.function_name
+  filename         = data.archive_file.task_payload_zip.output_path
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.handler"
+  runtime          = var.compatible_runtimes
+  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  source_code_hash = data.archive_file.task_payload_zip.output_base64sha256
+  publish          = true
+}
+
