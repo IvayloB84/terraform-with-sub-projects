3d84a3e3679fbe99332586ab1e432a79a5c4c6f9
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-03-30 16:47:40 +0300

    Update main.tf

--- sub1/main.tf
+++ sub1/main.tf
@@ -2,7 +2,7 @@ module "lambda" {
   source = "../modules/lambda"
     
   function_name = var.function_name
-  handler       = "index.lambda_handler"
+  handler       = var.lambda_handler
   runtime       = var.compatible_runtimes
   publish = true
 }
