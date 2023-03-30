6d3c668c73c81026aa96065d37d486f4bf543e5b
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-03-30 09:23:29 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,24 @@
 
+variable "description" {
+  description = "Description of the alias."
+  type        = string
+  default     = ""
+}
+
+variable "function_name" {
+  description = "The function ARN of the Lambda function for which you want to create an alias."
+  type        = string
+  default     = ""
+}
+
+variable "function_version" {
+  description = "Lambda function version for which you are creating the alias. Pattern: ($LATEST|[0-9]+)."
+  type        = string
+  default     = ""
+}
+
+variable "compatible_runtimes" {
+  description = "Default runtime for lambda"
+  type = string
+  default = "nodejs14.x"
+}
