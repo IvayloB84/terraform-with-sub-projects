57be730d50ce076f8ee5f46745d3756eed4870eb
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-03-30 14:50:18 +0300

    Update variables.tf

--- sub1/variables.tf
+++ sub1/variables.tf
@@ -1,13 +1,13 @@
 variable "description" {
   description = "Description of the alias."
   type        = string
-  default     = "A starter AWS Lambda function."
+  default     = "Not needed"
 }
 
 variable "function_name" {
   description = "The function ARN of the Lambda function for which you want to create an alias."
   type        = string
-  default     = "tf-lambda-for-module"
+  default     = "tf-lambda-git"
 }
 
 variable "function_version" {
@@ -20,4 +20,16 @@ variable "compatible_runtimes" {
   description = "Default runtime for lambda"
   type = string
   default = "nodejs14.x"
+}
+
+variable "iam_role_name" {
+  description = "Default runtime for lambda"
+  type = string
+  default = "task_payload"
+}
+
+variable "iam_policy_name" {
+  description = "Default runtime for lambda"
+  type = string
+  default = "AWSLambdaBasicExecutionRole-f81c3014-0f09"
 }
