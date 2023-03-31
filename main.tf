453a0b0739e89c194b411966e48f19f05167f282
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-03-31 12:46:45 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 module "lambda" {
   source = "./modules/lambda"
+  
+  function_name = "${var.function_name}"
+    iam_role_name = "${var.iam_role_name}"
+  iam_policy_name = "${var.iam_policy_name}"
+  lambda_handler       = "${var.lambda_handler}"
+  compatible_runtimes       = "${var.compatible_runtimes}"
+  
 }
