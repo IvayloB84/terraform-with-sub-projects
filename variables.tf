10c7f551635a6c74948c875af9571f2e5e685ca6
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-04-03 13:19:47 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "function_name" {
+  description = "The function ARN of the Lambda function for which you want to create an alias."
+  type        = string
+}
+
+variable "compatible_runtimes" {
+  description = "Default runtime for lambda"
+  type = string
+  default = ""
+}
+
+variable "iam_role_name" {
+  description = "Default runtime for lambda"
+  type = string
+}
+
+variable "iam_policy_name" {
+  description = "Default runtime for lambda"
+  type = string
+}
+
+variable "lambda_handler" {
+  description = "Lambda function handler"
+  type = string 
+}
+
+variable "publish" {
+  description = "Default publish role"
+  type = bool
+}
