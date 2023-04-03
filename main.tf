90609343efdbacb637888da9deafee2b2c7cfd15
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-04-03 13:23:09 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "lambda" {
-  source = "../modules/lambda"
+  source = "./modules/lambda"
     
   iam_role_name = var.iam_role_name
   iam_policy_name = var.iam_policy_name
