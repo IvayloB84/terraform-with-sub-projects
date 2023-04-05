7fec5fd8d99606e4a2853b180bece8921843a9bb
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-04-05 13:12:02 +0300

    Update main.tf

--- sub1/main.tf
+++ sub1/main.tf
@@ -10,7 +10,6 @@ module "lambda" {
   function_name = "tf-lambda-git"
   lambda_handler       = "index.handler"
   compatible_runtimes       = "nodejs14.x"
-  source_code_hash = data.archive_file.payload_zip.output_base64sha256
   
   ignore_source_code_hash = false
   publish = true
