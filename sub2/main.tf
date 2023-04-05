831660cb73cf085af2f469c8fc8a158e2fef1748
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-04-05 17:08:22 +0300

    Update main.tf

--- sub2/main.tf
+++ sub2/main.tf
@@ -9,8 +9,8 @@ module "lambda" {
   iam_role_name = "tf-lambda-git-role-1"     
   iam_policy_name = "tf-lambda-policy-name-1"
   function_name = "tf-lambda-git-1"
-  lambda_handler       = "index.handler"
-  compatible_runtimes       = "nodejs14.x"
+  lambda_handler       = "index.handler"     
+  compatible_runtimes       = "nodejs14.x"         
   
   publish = true
 }         
