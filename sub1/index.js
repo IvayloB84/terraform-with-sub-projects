e99da4a148503d79caa9d9fd4609013e0da77389
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-04-07 13:33:50 +0300

    Update index.js

--- sub1/index.js
+++ sub1/index.js
@@ -1,10 +1,10 @@
 console.log('Loading function');
 
 exports.handler = async (event, context) => {
-    //consol.log.new3('Received event:', JSON.stringify(event, null, 2));
+    //console.log.new3('Received event:', JSON.stringify(event, null, 2));
     console.log('value1 =', event.key1);
     console.log('value2 =', event.key2);     
     console.log('value3 =', event.key3);
     return event.key1;  //Echo back the first key value
-    // throw new Error('Something went wrong');    
+    // throw new Error('Something went wrong');
 };
