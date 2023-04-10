62c1af30b0cc593b55505dd14effe63b045d2aea
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-04-10 06:15:10 +0300

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -47,6 +47,11 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 }
 
 resource "null_resource" "lambda_dependencies" {
+  
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
   provisioner "local-exec" {
     command = "mkdir -p ./lambda  && cp index.js ./lambda && cd ./lambda && npm install --legacy-peer-deps"
   }
