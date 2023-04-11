63868f2b2ee4e748cfcb51da46074c434351b6b7
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-04-11 15:36:31 +0300

    new update

--- sub7/index.js
+++ sub7/index.js
@@ -7,4 +7,4 @@ exports.handler = async (event, context) => {
     console.log('value3 =', event.key3);
     return event.key1;  //Echo back the first key value
     // throw new Error('Something went wrong');
-};   
+};
