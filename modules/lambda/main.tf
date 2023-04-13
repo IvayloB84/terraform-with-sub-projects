c374fdd5916f9e997e9d7a6e31f94c6fae5ac8a8
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-04-13 11:16:49 +0300

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -53,7 +53,7 @@ resource "null_resource" "lambda_dependencies" {
 //  }
 
   provisioner "local-exec" {
-    command = "mkdir -p ./lambda  && cp -Ruv *{js,json} ./lambda && cd ./lambda && npm install --legacy-peer-deps"
+    command = "mkdir -p ./lambda  && cp *{js,json} ./lambda && cd ./lambda && npm install --legacy-peer-deps"
   }
 }
 
