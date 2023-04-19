bf7e3685012babb748d644d13f8ef311639edd7c
saywhat <72907892+IvayloB84@users.noreply.github.com>
2023-04-19 13:02:01 +0300

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -69,11 +69,11 @@ data "archive_file" "payload_zip" {
 }
         
 resource "aws_lambda_function" "payload" {
-  function_name    = "${var.function_name}"
-//  filename         = data.archive_file.payload_zip.output_path
-  filename         = "./payload.zip"
+  function_name    = var.function_name
+  filename         = data.archive_file.payload_zip.output_path
+//  filename         = "./payload.zip"
   role             = aws_iam_role.payload.arn
-  handler          = "${var.lambda_handler}"
+  handler          = "var.lambda_handler
   runtime          = "${var.compatible_runtimes}"
   depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]    
 //  source_code_hash = data.archive_file.payload_zip.output_base64sha256
