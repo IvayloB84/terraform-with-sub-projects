e3781f4e79888131849bb3cc3c633091e0860763
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-04-20 12:25:35 +0300

    update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -52,11 +52,10 @@ resource "null_resource" "lambda_dependencies" {
     package = "${base64sha256(file("./package.json"))}"
     lock    = "${base64sha256(file("./package-lock.json"))}"
   }
-}
-
-  provisioner "local-exec" {
+    provisioner "local-exec" {
     command = "mkdir -p ./lambda && cd ./lambda && npm install --legacy-peer-deps"
   }
+}
 
 data "archive_file" "payload_zip" {
   type        = "zip"
