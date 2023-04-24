1dec1e8bc1cdfe5be12d6d97f864f9fa592ab072
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-04-24 17:44:30 +0300

    update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -50,7 +50,7 @@ resource "null_resource" "lambda_dependencies" {
 
   provisioner "local-exec" {
 //    command = "mkdir -p ./lambda && cd ./lambda && cp -u ../index.js . && npm install --legacy-peer-deps && cd -"  
-      command = "mkdir -p ./lambda && rsync -av --exclude={'*.tf','*.tfstate*','*./*','*terraform*','lambda/','*.zip'} ./ lambda/ && cd ./lambda && npm install --legacy-peer-deps"
+      command = "mkdir -p ./lambda && rsync -av --exclude={'*.tf','*.tfstate*','*./*','*terraform*','lambda/','*.zip'} ./ ./lambda/ && cd ./lambda && npm install --legacy-peer-deps"
   }
 }
 
