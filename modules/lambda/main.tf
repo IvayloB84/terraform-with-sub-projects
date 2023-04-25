c561f1215863b73d5bc776d4fd05c10237b4472a
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-04-25 08:23:56 +0300

    update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -56,9 +56,9 @@ resource "null_resource" "lambda_dependencies" {
 
  data "archive_file" "payload_zip" {
   type        = "zip"
-  source_dir = "../${var.dir}/lambda"
+  source_dir = "./${var.dir}/lambda"
 //  source_file = "../${var.dir}/index.js"
-  output_path = "../${var.dir}/payload.zip"
+  output_path = "./${var.dir}/payload.zip"
   depends_on  = [null_resource.lambda_dependencies]
 } 
 
