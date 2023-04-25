0facf1a2d4c34e321d38dab8f4e4848d5df53d18
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-04-25 13:34:47 +0300

    update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -56,8 +56,8 @@ resource "null_resource" "lambda_dependencies" {
 
 data "archive_file" "payload_zip" {
   type        = "zip"
-  source_dir  = "lambda/"
-  output_path = "payload.zip"
+  source_dir  = "../lambda/"
+  output_path = "../payload.zip"
   depends_on  = [null_resource.lambda_dependencies]
 }
 
