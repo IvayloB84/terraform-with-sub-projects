d553fc146b1654d54324ce6a29af6098015302bd
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-04-25 10:43:59 +0300

    update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -65,7 +65,7 @@ data "null_data_source" "wait_for_lambda_exporter" {
  data "archive_file" "payload_zip" {
   type        = "zip"
 //  source_dir = "lambda/"
-  source_dir = "${data.null_data_source.wait_for_lambda_exporter.outputs["lambda/"]}"
+  source_dir = "${data.null_data_source.wait_for_lambda_exporter.outputs["source_dir"]}"
   output_path = "payload.zip"
   depends_on  = [
     null_resource.lambda_dependencies
