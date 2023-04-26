49e804261f291396258fa4f8fe0226dfeb012d86
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-04-26 11:52:07 +0300

    update

--- sub1/main.tf
+++ sub1/main.tf
@@ -6,7 +6,7 @@ module "lambda" {
   source = "../modules/lambda"
 
   iam_role_name       = "tf-lambda-iam-role-sub1"
-  iam_policy_name     = "tf-lambda-policy-name"
+  iam_policy_name     = "tf-lambda-policy-sub1"
   function_name       = "tf-lambda-sub1"
   lambda_handler      = "index.handler"
   compatible_runtimes = "nodejs14.x"
