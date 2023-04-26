a5002e43aca138294b20eaaa6e6c7986be6faccf
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-04-26 08:40:18 +0300

    update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -75,7 +75,7 @@ resource "aws_lambda_function" "payload" {
   runtime       = "${var.compatible_runtimes}"
   depends_on = [
     aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role,
-    null_resource.lambda_dependencies,
+    data.archive_file.payload_zip,
   ]
   //source_code_hash = "${data.archive_file.payload_zip.output_base64sha256}"
   publish          = true
