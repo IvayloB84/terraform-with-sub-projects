43008f728f9affa85437fa5b960d3f0e16022e8e
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-04-27 15:45:53 +0300

    update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -49,7 +49,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 resource "null_resource" "lambda_dependencies" {
 
   triggers = {
-    policy_sha1 = "${sha1(file("./${var.dir}/"))}"
+    dir_sha1 = sha1(join("", [for f in fileset(path.cwd, "*"): filesha1(f)]))
   }
 
 /*   triggers = {
