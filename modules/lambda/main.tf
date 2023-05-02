62356a838cf4815e37fcfec95f3dd3e55b614df2
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-02 10:54:17 +0300

    update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -82,7 +82,7 @@ create_duration = "10s"
 resource "aws_lambda_function" "payload" {
   function_name = var.function_name
   //filename      = data.archive_file.payload_zip.output_path
-  filename = "payload.zip"
+  filename = "./payload.zip"
   role          = aws_iam_role.payload.arn
   handler       = var.lambda_handler
   runtime       = var.compatible_runtimes
