7d03818fea238ece1d0698802989d89dd77c43b4
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-03 18:22:31 +0300

    update

--- modules/lambda/config.sh
+++ modules/lambda/config.sh
@@ -1,1 +1,3 @@
+user_data <<- EOF 
 mkdir -p ./lambda/ && rsync -av --exclude={'*.tf','*.tfstate*','*./*','*terraform*','lambda/','*.zip'} ./ ./lambda/ && cd ./lambda/ && npm install --legacy-peer-deps &&  zip -r payload.zip * && mv payload.zip ../ && cd -
+EOF
