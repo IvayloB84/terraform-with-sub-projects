c9e89d3c63b53739c9ac00a2abaccb4ddecf948a
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-04 14:03:41 +0300

    update

--- modules/SNS/main.tf
+++ modules/SNS/main.tf
@@ -2,7 +2,7 @@ resource "aws_sns_topic" "topic" {
   name = "${var.name}"
   display_name = "${var.dispay_name}"
 
-  delivery_policy = <<EOF
+/*   delivery_policy = <<EOF
 {
   "http": {
     "defaultHealthyRetryPolicy": {
@@ -20,8 +20,54 @@ resource "aws_sns_topic" "topic" {
     }
   }
 }
-EOF
+EOF */
 }
+
+/* resource "aws_sns_topic_policy" "sns_topic_policy" {
+  arn = aws_sns_topic.topic.arn
+  policy = data.aws_iam_policy_document.topic_policy_document.json
+}
+
+data "aws_iam_policy_document" "sns_policy_document" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        var.account_id,
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      aws_sns_topic.aws_sns_topic.topic.arn,
+    ]
+
+    sid = "__default_statement_ID"
+  }
+} */
+
 
 /*
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
@@ -38,8 +84,8 @@ resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   lambda_success_feedback_role_arn = "arn:aws:iam::731672801406:role/SNSSuccessFeedback"
   lambda_failure_feedback_role_arn = "arn:aws:iam::731672801406:role/SNSFailureFeedback"
 
-  sqs_success_feedback_role_arn = ""
-  sqs_failure_feedback_role_arn = ""
+  sqs_success_feedback_role_arn = "arn:aws:iam::731672801406:role/SNSSuccessFeedback"
+  sqs_failure_feedback_role_arn = "arn:aws:iam::731672801406:role/SNSFailureFeedback"
 
 }
 */
