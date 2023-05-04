66884505506febde629bdd2f543bd5968daba513
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-04 11:23:05 +0300

    SNS module for lambda functions

---
+++ modules/SNS/variables.tf
@@ -0,0 +1,26 @@
+variable "name" {
+  description = "Name of the SNS topic"
+  type        = string
+}
+
+variable "dispay_name" {
+  description = "Display name"
+  type        = string
+}
+
+/*
+variable "topic.arn" {
+  description = "ARN of the SNS topic"
+  type        = string
+}
+
+variable "protocol" {
+  description = "Protocol of the SNS topic"
+  type        = string
+}
+
+variable "endpoint" {
+  description = "Protocol of the SNS topic"
+  type        = string
+}
+*/
