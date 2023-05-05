149ee1bde3d6165849149f94083e2ade141005da
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-05 18:17:28 +0300

    update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -62,7 +62,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 /*     command = "rm -rf ./lambda payload.zip && mkdir -p ./lambda/ && rsync -av --exclude={'*.tf','*.tfstate*','*./*','*terraform*','lambda/','*.zip'} ./ ./lambda/ && cd ./lambda/ && npm install --legacy-peer-deps && cd -"
 //   interpreter = ["/bin/bash", "-c"] 
 */
-command = "mkdir -p lambda && npm install --legacy-peer-deps && pwd"
+command = "npm install --legacy-peer-deps && pwd"
     }
 
 /*       triggers = {
@@ -71,8 +71,8 @@ command = "mkdir -p lambda && npm install --legacy-peer-deps && pwd"
  }
   data "archive_file" "payload_zip" {
   type        = "zip"
-  source_dir  = "./lambda"
-  output_path = "./lambda/payload.zip"   
+  source_dir  = "./${var.dir}"
+  output_path = "./payload.zip"   
   excludes = [
     "*.terraform*",
     "*.tfstate",
