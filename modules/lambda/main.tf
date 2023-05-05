2bcbe84b930250c961bf305fedb0afeb40f3ea59
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-05 16:58:44 +0300

    update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -60,7 +60,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
   provisioner "local-exec" {
 /*     command = "rm -rf ./lambda payload.zip && mkdir -p ./lambda/ && rsync -av --exclude={'*.tf','*.tfstate*','*./*','*terraform*','lambda/','*.zip'} ./ ./lambda/ && cd ./lambda/ && npm install --legacy-peer-deps && cd -"
 //   interpreter = ["/bin/bash", "-c"] */
-command = "mkdir -p ./lambda/ && cp -p index.js && cd ./lambda/ && npm install --legacy-peer-deps && cd -"
+command = "mkdir -p ./lambda/ && cp *.js *.json ./lambda && cd ./lambda/ && npm install --legacy-peer-deps && cd -"
     }
 
 /*       triggers = {
