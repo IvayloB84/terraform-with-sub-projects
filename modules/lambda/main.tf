4399527ccefbdfdae591ca0c4b19ec8a9c988fe3
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-09 11:40:43 +0300

    update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 resource "aws_iam_role" "payload" {
-  name = var.iam_role_name
+  name = "${var.iam_role_name}"
 
   assume_role_policy = <<EOF
   
@@ -24,7 +24,7 @@ EOF
 
 resource "aws_iam_policy" "AWSLambdaBasicExecutionRole" {
 
-  name        = var.iam_policy_name
+  name        = "${var.iam_policy_name}"
   path        = "/"
   description = "AWS IAM Policy for managing aws lambda role"
   policy      = <<EOF
@@ -54,17 +54,13 @@ resource "null_resource" "archive" {
 
   triggers = {
     dependencies_versions = filemd5("./index.js")
+    create_file = fileexists("./readme.txt")
+    updated_at = timestamp()
   }
 
   /*     triggers = {
     updated_at = timestamp()
   } 
-
-
-  triggers = {
-    create_file = fileexists("./readme.txt")
-    updated_at = timestamp()
-  }
 */
 
   provisioner "local-exec" {
@@ -87,8 +83,8 @@ resource "random_uuid" "lambda_src_hash" {
 
 data "archive_file" "payload_zip" {
   type        = "zip"
-  source_dir  = "./lambda"
-  output_path = "./payload.zip"
+  source_dir  = local.lambda_src_path
+  output_path = "${random_uuid.lambda_src_hash.result}.zip"
   excludes = [
     "*.terraform*",
     "*.tfstate",
@@ -110,16 +106,15 @@ resource "time_sleep" "wait_20_seconds" {
 }
 
 resource "aws_lambda_function" "payload" {
-  function_name = var.function_name
+  function_name = "${var.function_name}"
   filename      = data.archive_file.payload_zip.output_path
   role          = aws_iam_role.payload.arn
-  handler       = var.lambda_handler
-  runtime       = var.compatible_runtimes
+  handler       = "${var.lambda_handler}"
+  runtime       = "${var.compatible_runtimes}"
   timeout       = 90
   depends_on = [
     aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role,
     data.archive_file.payload_zip,
-    time_sleep.wait_20_seconds
   ]
 
   source_code_hash = data.archive_file.payload_zip.output_base64sha256
