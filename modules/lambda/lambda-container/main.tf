3367264aaeb25243df4ab9ad257888f154bbb9b0
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-10 15:01:14 +0300

    update

--- modules/lambda/lambda-container/main.tf
+++ modules/lambda/lambda-container/main.tf
@@ -15,6 +15,30 @@ resource "aws_ecr_repository" "image_demo_lambda_repository" {
   force_delete = true
 }
 
+resource "aws_ecr_lifecycle_policy" "foopolicy" {
+  repository = aws_ecr_repository.image_demo_lambda_repository.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 7 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 7
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
 resource "null_resource" "container_image_requirements" {
   triggers = {
     nodejs_file = md5(file("./index.js"))
