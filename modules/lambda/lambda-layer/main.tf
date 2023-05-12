4e0e638bca4c109e656c2c31c2f15ab2dbe692e3
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-12 12:09:54 +0300

    update

--- modules/lambda/lambda-layer/main.tf
+++ modules/lambda/lambda-layer/main.tf
@@ -1,14 +1,14 @@
 data "aws_caller_identity" "current" {}
 
 locals {
-  destination_dir = "${path.module}/../layers/${var.layer_name}"
+  destination_dir = "./layers/${var.layer_name}"
 }
 
 resource "null_resource" "layer_dependencies" {
   provisioner "local-exec" {
     command = "mkdir -p ./nodejs/ && rsync -av --exclude={'*.tf','*.tfstate*','*./*','*terraform*','lambda/','*.zip'} ./ ./nodejs/ && cd nodejs && npm install --legacy-peer-deps"
     interpreter = ["/bin/bash", "-c"]
-    working_dir = "${local.destination_dir}"
+    working_dir = "${local.destination_dir}/${var.layer_name}"
   } 
 }
 
@@ -22,7 +22,7 @@ resource "aws_lambda_layer_version" "lambda_layers" {
   filename   = "${local.destination_dir}/${var.layer_name}.zip"
   layer_name = var.layer_name
 
-  compatible_runtimes = ["nodejs14.x", "nodejs16.x"]
+  compatible_runtimes = ["nodejs14.x","nodejs16.x"]
 
   depends_on = [
     data.archive_file.local_archive
