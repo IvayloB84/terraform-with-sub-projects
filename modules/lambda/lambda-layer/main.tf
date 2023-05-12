a628c658b6f4c2c90015b2a735262fd92356ba62
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-12 14:07:30 +0300

    update

--- modules/lambda/lambda-layer/main.tf
+++ modules/lambda/lambda-layer/main.tf
@@ -1,20 +1,20 @@
 data "aws_caller_identity" "current" {}
 
 locals {
-  layer_src_path = "./source"
+  layer_src_path = "${var.dir}/source"
   destination_dir = "${path.module}/layers/${var.layer_name}"
 }
 
 resource "null_resource" "layer_dependencies" {
   provisioner "local-exec" {
-    command = "mkdir -p ./source/nodejs/ && rsync -av --exclude={'*.tf','*.tfstate*','*./*','*terraform*','lambda/','*.zip','source/'} ./ ./source/nodejs/ && cd ./source/nodejs/ && npm install --legacy-peer-deps"
+    command = "mkdir -p ./source/nodejs/ && rsync -av --exclude={'*.tf','*.tfstate*','*./*','*terraform*','lambda/','*.zip','source/'}./ ./source/nodejs/ && cd ./source/nodejs/ && npm install --legacy-peer-deps && cd -"
     interpreter = ["/bin/bash", "-c"]
   } 
 }
 
 data "archive_file" "local_archive" {
   type        = "zip"
-  source_dir  = "./source"
+  source_dir  = "${local.layer_src_path}"
   output_path = "${local.destination_dir}/${var.layer_name}.zip"
   depends_on = [ 
     null_resource.layer_dependencies
