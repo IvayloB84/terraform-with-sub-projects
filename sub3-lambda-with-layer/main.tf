874a9642c269e949921b87dcdeaaf9d32325a030
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-12 11:59:44 +0300

    update

--- sub3-lambda-with-layer/main.tf
+++ sub3-lambda-with-layer/main.tf
@@ -5,12 +5,12 @@ provider "aws" {
 module "lambda-layer" {
   source = "../modules/lambda/lambda-layer"
 
-  iam_role_name       = "tf-lambda-iam-role-sub3"
-  iam_policy_name     = "tf-lambda-policy-sub3"
-  function_name       = "tf-lambda-sub3"
-  description         = "Lambda function + layer created with Terraform"
-  lambda_handler      = "index.handler"
+  # iam_role_name       = "tf-lambda-iam-role-sub3"
+  # iam_policy_name     = "tf-lambda-policy-sub3"
+  # function_name       = "tf-lambda-sub3"
+  # description         = "Lambda function + layer created with Terraform"
+  # lambda_handler      = "index.handler"
   layer_name          = "sub3-lambda-with-layer"
-  compatible_runtimes = "nodejs14.x"
-  dir                 = "sub3-lambda-with-layer"
+  # compatible_runtimes = "nodejs14.x"
+  # dir                 = "sub3-lambda-with-layer"
   }
