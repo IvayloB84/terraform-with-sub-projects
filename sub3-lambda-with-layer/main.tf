502b51657b611b6c3c9b9badfd3b1eadda9c99e2
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-12 15:11:52 +0300

    update

--- sub3-lambda-with-layer/main.tf
+++ sub3-lambda-with-layer/main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "us-west-2"
 }
 
-locals {
-  layer_name = "${modules.lambda.lambda-layers.layer_name}"
-}
-
 module "lambda-layer" {
   source = "../modules/lambda/lambda-layer"
 
-  layer_name          = "sub3-lambda-with-layer"
+  layer =  "sub3-lambda-with-layer"
   function_name       = "tf-lambda-sub3"
   description         = "Lambda function + layer created with Terraform"
   }
+
+  output "function_name" {
+ value = "${modules.lambda.function_name}"  
+}
