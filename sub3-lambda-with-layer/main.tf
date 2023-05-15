5072fae264e910d092ed2a892148412b3b689490
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-15 08:32:48 +0300

    update

--- sub3-lambda-with-layer/main.tf
+++ sub3-lambda-with-layer/main.tf
@@ -10,14 +10,20 @@ locals {
    vpc_id = "${modules.my_vpc.vpc_id}"
 }
 
+
+resource "time_sleep" "wait_20_seconds" {
+
+  create_duration = "20s"
+}
+
 module "lambda-layer" {
   source = "../modules/lambda/lambda-layer"
 
   layer_name =  "sub3-lambda-with-layer"
   function_name       = "tf-lambda-sub3"
   description         = "Lambda function + layer created with Terraform"
-  }
 
-  output "function_name" {
- value = "${modules.lambda.function_name}"  
-}
+  create_layer = true
+
+  depends_on = [ time_sleep.wait_20_seconds ]
+  }
