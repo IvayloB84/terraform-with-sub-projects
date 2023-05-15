d7798b0357fcf5daccee822513fc5bcffea51e6f
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-15 13:00:35 +0300

    Lambda with if else condition

---
+++ lambda-with-if-else-statement/main.tf
@@ -0,0 +1,121 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+locals {
+  lambda_src_path = "./lambda"
+  create = var.create
+}
+
+resource "aws_iam_role" "payload" {
+  name = var.iam_role_name
+
+  assume_role_policy = <<EOF
+  
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"    
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "AWSLambdaBasicExecutionRole" {
+
+  name        = var.iam_policy_name
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.payload.name
+  policy_arn = aws_iam_policy.AWSLambdaBasicExecutionRole.arn
+}
+
+resource "null_resource" "archive" {
+
+  triggers = {
+    dependencies_versions = filemd5("./index.js")
+    create_file           = fileexists("./readme.txt")
+    updated_at            = timestamp()
+
+  }
+
+  provisioner "local-exec" {
+
+    command     = "mkdir -p ./lambda/ && rsync -av --exclude={'*.tf','*.tfstate*','*./*','*terraform*','lambda/','*.zip'} ./ ./lambda/ && cd ./lambda && npm install --legacy-peer-deps"
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+data "archive_file" "payload_zip" {
+  type        = "zip"
+  source_dir  = local.lambda_src_path
+  output_path = "${var.function_name}-payload.zip"
+  /*   excludes = [
+    "*.terraform*",
+    "*.tfstate",
+    "*.tf",
+    "payload.zip",
+    "lambda",
+    "*./*"
+  ] */
+
+  depends_on = [
+    null_resource.archive
+  ]
+}
+
+resource "time_sleep" "wait_20_seconds" {
+  depends_on = [
+    null_resource.archive
+  ]
+
+  create_duration = "20s"
+}
+
+resource "aws_lambda_function" "payload" {
+  count = local.create && var.create_function && !var.create_layer ? 1 : 0
+  
+  function_name = var.create_function
+  filename      = data.archive_file.payload_zip.output_path
+  description   = var.description
+  role          = aws_iam_role.payload.arn
+/*   layers = [
+    module.lambda_layers.lambda_layers_arn
+    ] */
+  handler       = var.lambda_handler
+  runtime       = var.compatible_runtimes
+  timeout       = 90
+
+  depends_on = [
+    aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role,
+    data.archive_file.payload_zip,
+  ]
+
+  source_code_hash = data.archive_file.payload_zip.output_base64sha256
+  publish          = true
+}
---
+++ lambda-with-if-else-statement/variables.tf
@@ -0,0 +1,53 @@
+variable "create" {
+  description = "Controls whether resources should be created"
+  type        = bool
+  default     = true
+}
+
+variable "create_function" {
+  description = "Controls whether Lambda Function resource should be created"
+  type        = bool
+  default     = true
+}
+
+variable "create_layer" {
+  description = "Controls whether Lambda Layer resource should be created"
+  type        = bool
+  default     = false
+}
+
+variable "description" {
+  description = "The function ARN of the Lambda function for which you want to create an alias."
+  type        = string
+  default     = "If else statement test"
+}
+
+variable "compatible_runtimes" {
+  description = "Default runtime for lambda"
+  type        = string
+  default = "nodejs14.x"
+}
+
+variable "iam_role_name" {
+  description = "Default runtime for lambda"
+  type        = string
+  default = "some-new-tf-iam-role"
+}
+
+variable "iam_policy_name" {
+  description = "Default runtime for lambda"
+  type        = string
+  default = "some-new-tf-iam-policy-name"
+}
+
+variable "lambda_handler" {
+  description = "Lambda function handler"
+  type        = string
+  default = "index.handler"
+}
+
+variable "layer_name" {
+  description = "Layer for Lambda function"
+  type        = string
+  default     = ""
+}
