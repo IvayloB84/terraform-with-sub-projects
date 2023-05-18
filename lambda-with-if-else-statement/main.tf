382c426907cd3d3279feb9e8688891ee79cdf0a6
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-18 14:56:50 +0300

    update

--- lambda-with-if-else-statement/main.tf
+++ lambda-with-if-else-statement/main.tf
@@ -101,7 +101,7 @@ resource "aws_lambda_function" "payload" {
   description   = var.description
   role          = aws_iam_role.payload.arn
   layers        = [
-    aws_lambda_layer_version.lambda_layers[index.count].arn,
+    aws_lambda_layer_version.lambda_layers[*].arn,
   ]
   handler       = var.lambda_handler
   runtime       = var.compatible_runtimes
