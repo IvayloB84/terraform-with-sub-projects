c060218c1acd5a5dbc9bc594a0b52477d6f7aed1
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-19 15:03:42 +0300

    update

--- lambda-with-if-else-statement/main.tf
+++ lambda-with-if-else-statement/main.tf
@@ -151,7 +151,7 @@ resource "aws_lambda_layer_version" "lambda_layers" {
   count = var.create && var.create_layer ? 0 : 1
 
   filename            = "./${var.layer_name}-layer.zip"
-  layer_name          = var.layer_name
+  layer_name          = try(var.layer_name[0], null)
   source_code_hash    = filebase64sha256("./${var.layer_name}-layer.zip")
   compatible_runtimes = ["nodejs14.x", "nodejs16.x"]
 
