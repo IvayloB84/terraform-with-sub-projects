e6ad7d2fd6653203b5561fad3f66d35212eff0b4
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-19 18:03:32 +0300

    update

--- modules/lambda/lambda-layers/main.tf
+++ modules/lambda/lambda-layers/main.tf
@@ -45,9 +45,11 @@ resource "aws_lambda_layer_version" "lambda_layers" {
   count = var.create && var.create_layer ? 1 : 0
 
   //  filename   = "${local.destination_dir}/${var.layer_name}.zip"
-  filename            = "${local.destination_dir}/${var.layer_name}-layer.zip"
+  //filename            = "${local.destination_dir}/${var.layer_name}-layer.zip"
+  filename = data.archive_file.local_archive.output_path
   layer_name          = var.layer_name
-  source_code_hash    = filebase64sha256("${local.destination_dir}/${var.layer_name}-layer.zip")
+  // source_code_hash    = filebase64sha256("${local.destination_dir}/${var.layer_name}-layer.zip")
+  source_code_hash = data.archive_file.local_archive.output_base64sha256
   compatible_runtimes = ["nodejs14.x", "nodejs16.x"]
 
   depends_on = [
