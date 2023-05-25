8eb779a257c9588dabf27c85c67d8a1cfb39f325
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-25 12:23:19 +0300

    update

--- lambda-with-if-else-statement/main.tf
+++ lambda-with-if-else-statement/main.tf
@@ -148,7 +148,8 @@ resource "time_sleep" "wait_20_seconds" {
 }
 
 resource "aws_lambda_layer_version" "lambda_layers" {
-  count = var.create && var.create_layer ? 0 : 1
+  count = var.create && var.create_function && var.create_layer ? 1 : 0
+//  count = var.create && var.create_layer ? 0 : 1
 
   filename            = "./${var.layer_name}-layer.zip"
   layer_name          = var.layer_name
