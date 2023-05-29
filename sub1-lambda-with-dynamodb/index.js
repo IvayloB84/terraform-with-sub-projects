03fdeee69f27aa329e90f994ddf7077361a7ec78
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-05-29 12:13:52 +0300

    update

---
+++ sub1-lambda-with-dynamodb/index.js
@@ -0,0 +1,10 @@
+console.log('Loading function');
+
+exports.handler = async (event, context) => {
+    //console.log.1 ('Received event:', JSON.stringify(event, null, 2));
+    console.log('value1 =', event.key1);
+    console.log('value2 =', event.key2);     
+    console.log('value3 =', event.key3);
+    return event.key1;  //Echo back the first key value
+    // throw new Error('Also commented on 11.05.2023');
+};
---
+++ sub1-lambda-with-dynamodb/main.tf
@@ -0,0 +1,49 @@
+data "aws_caller_identity" "current" {}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_dynamodb_table" "basic-db-table" {
+    name = "tf-dynamodb"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "Id"
+    stream_enabled   = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+
+    attribute {
+      name = "Id"
+      type = "S"
+    }
+}
+/*
+resource "aws_lambda_function" "tf-lambda-with-dynamodb" {
+  function_name    = "tf-lambda-with-dynamodb"
+  filename         = data.archive_file.lambda_zip_file.output_path
+  source_code_hash = data.archive_file.lambda_zip_file.output_base64sha256
+  handler          = "handler.handler"
+  role             = aws_iam_role.lambda_assume_role.arn
+  runtime          = "python3.8"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "archive_file" "lambda_zip_file" {
+  output_path = "${path.module}/lambda_zip/lambda.zip"
+  source_dir  = "${path.module}/../lambda"
+  excludes    = ["__init__.py", "*.pyc"]
+  type        = "zip"
+}
+
+ module "lambda" {
+  source = "../modules/lambda"
+
+  iam_role_name       = "tf-lambda-iam-role-sub1"
+  iam_policy_name     = "tf-lambda-policy-sub1"
+  function_name       = "tf-lambda-sub1"
+  lambda_handler      = "index.handler"
+  compatible_runtimes = "nodejs14.x"
+  dir                 = "sub1"
+} */
