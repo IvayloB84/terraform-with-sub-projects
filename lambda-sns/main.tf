cb045526e7b32225446242615fadae681afeda0f
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-06-01 12:11:38 +0300

    update

--- lambda-sns/main.tf
+++ lambda-sns/main.tf
@@ -4,7 +4,53 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
+
+
 resource "aws_sns_topic" "user_updates" {
   name = "tf-sns-topic"
 }
 
+resource "aws_sns_topic_policy" "default" {
+  arn = aws_sns_topic.test.arn
+
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        var.account-id,
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      aws_sns_topic.user_updates.arn,
+    ]
+
+    sid = "__default_statement_ID"
+  }
+}
