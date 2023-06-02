c692eb74c7b07c0c761d702dd2a3936069c9f2a2
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-06-02 09:36:18 +0300

    update

--- lambda-sns/main.tf
+++ lambda-sns/main.tf
@@ -7,10 +7,10 @@ data "aws_caller_identity" "current" {}
 locals {
   lambda_src_path = "./lambda"
 }
-
+/* 
 resource "aws_sns_topic" "user_updates" {
   name = "tf-sns-topic"
-}
+} */
 
 resource "aws_iam_role" "payload" {
   name = var.iam_role_name
@@ -71,11 +71,11 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 }
 
 
-resource "aws_lambda_event_source_mapping" "tf-source" {
+/* resource "aws_lambda_event_source_mapping" "tf-source" {
   event_source_arn  = aws_sns_topic.user_updates.arn
   function_name     = aws_lambda_function.TF-if-else-statement.arn
   starting_position = "LATEST"
-}
+} */
 
 resource "null_resource" "archive" {
 
@@ -128,10 +128,4 @@ resource "aws_lambda_function" "TF-if-else-statement" {
 
   source_code_hash = data.archive_file.payload_zip.output_base64sha256
   publish          = true
-}
-
-resource "aws_lambda_event_source_mapping" "source_mapping" {
-  event_source_arn  = aws_sns_topic.user_updates.arn
-  function_name     = aws_lambda_function.TF-if-else-statement.arn
-  starting_position = "LATEST"
 }
