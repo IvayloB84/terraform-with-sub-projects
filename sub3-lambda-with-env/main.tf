856626d47317befa9ab9aef86dc99f1ddf04f2e2
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-06-14 12:42:57 +0300

    update

--- sub3-lambda-with-env/main.tf
+++ sub3-lambda-with-env/main.tf
@@ -126,7 +126,8 @@ data "aws_lambda_alias" "production" {
 }
 
 resource "aws_lambda_alias" "test_lambda_alias" {
-  name             = "${var.env_names}"
+  for_each = var.env_names
+  name             = each.value
   description      = "a sample description"
   function_name    = aws_lambda_function.payload.arn
   function_version = "1"
