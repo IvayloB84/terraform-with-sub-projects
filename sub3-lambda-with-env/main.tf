d1a05dee4faa7e3af47b4d96fa1554fb9435114a
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-06-15 11:37:44 +0300

    update

--- sub3-lambda-with-env/main.tf
+++ sub3-lambda-with-env/main.tf
@@ -127,7 +127,7 @@ data "aws_lambda_function" "lambda" {
 
  data "aws_lambda_alias" "latest" {
   function_name = var.function_name
-  name          = ""
+  name          = "dev"
 } 
 
 resource "aws_lambda_alias" "test_lambda_alias" {
