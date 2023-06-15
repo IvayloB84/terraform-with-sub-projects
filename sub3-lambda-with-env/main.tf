48878ad95f8143532c0ddd74f354766e1d00ae9b
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-06-15 14:52:03 +0300

    update

--- sub3-lambda-with-env/main.tf
+++ sub3-lambda-with-env/main.tf
@@ -122,7 +122,7 @@ resource "aws_lambda_function" "payload" {
 
 data "aws_lambda_function" "lambda" {
   function_name = var.function_name
-  qualifier = data.aws_lambda_alias.latest.function_version
+  qualifier = data.aws_lambda_alias.latest.qualified.arn
 }
 
  data "aws_lambda_alias" "latest" {
