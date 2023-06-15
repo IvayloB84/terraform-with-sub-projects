9e82cd5393dd438ef5a3c1bc9eb34dde0b6db69b
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-06-15 15:14:52 +0300

    update

--- sub3-lambda-with-env/main.tf
+++ sub3-lambda-with-env/main.tf
@@ -135,7 +135,7 @@ resource "aws_lambda_alias" "test_lambda_alias" {
   name             = each.value
   description      = "a sample description"
   function_name    = var.function_name
-  function_version = data.aws_lambda_function.lambda
+//  function_version = data.aws_lambda_function.lambda
 
   depends_on = [ 
     data.aws_lambda_alias.latest
