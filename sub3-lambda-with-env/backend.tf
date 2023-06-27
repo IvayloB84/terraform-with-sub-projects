c03cdd9839f3bcd7f23f82334e79d321321568e7
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-06-27 14:13:10 +0300

    update

--- sub3-lambda-with-env/backend.tf
+++ sub3-lambda-with-env/backend.tf
@@ -1,8 +1,12 @@
+locals {
+  aws_s3_bucket = var.function_name
+} 
+ 
  terraform {
    backend "s3" {
      # Bucket name, key tag and AZ
      bucket = "sprintray-tf-state-files"
-     key    = "global/s3/${var.function_name}/terraform.tfstate"
+     key    = "global/s3/${local.aws_s3_bucket}/terraform.tfstate"
      region = "us-west-2"
     
      # DybamoDB table name
