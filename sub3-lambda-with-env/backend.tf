e933836269f29d417b064590152f0c7966e1d310
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-06-27 15:25:46 +0300

    update

--- sub3-lambda-with-env/backend.tf
+++ sub3-lambda-with-env/backend.tf
@@ -1,12 +1,8 @@
-locals {
-  aws_s3_bucket = var.function_name
-} 
- 
- terraform {
+terraform {
    backend "s3" {
      # Bucket name, key tag and AZ
      bucket = "sprintray-tf-state-files"
-     key    = "global/s3/${local.aws_s3_bucket}/terraform.tfstate"
+     key    = "global/s3/terraform.tfstate"
      region = "us-west-2"
     
      # DybamoDB table name
--- sub3-lambda-with-env/dynamodb.tf
+++ sub3-lambda-with-env/dynamodb.tf
@@ -1,13 +1,9 @@
-resource "aws_dynamodb_table" "terraform-lock" {
-    name           = "terraform_state"
-    read_capacity  = 5
-    write_capacity = 5
-    hash_key       = "LockID"
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
-    tags = {
-        "Name" = "DynamoDB Terraform State Lock Table"
-    }
+resource "aws_dynamodb_table" "state_locking" {
+  hash_key = "LockID"
+  name     = "dynamodb-terraform-state-locking"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  billing_mode = "PAY_PER_REQUEST"
 }
