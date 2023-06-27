f8a99d726da4df57a541c39db683e363a92e04f9
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-06-27 14:04:53 +0300

    update

--- sub3-lambda-with-env/index.js
+++ sub3-lambda-with-env/index.js
@@ -3,7 +3,7 @@ exports.handler = (event, context, callback) => {
 
   const date1 = moment(event["queryStringParameters"]["date1"].split('-'));
   const date2 = moment(event["queryStringParameters"]["date2"].split('-'));
-//Commented on 27.06.2023 
+//Commented on 27.06.2023 update
     const diff = date2.diff(date1, 'days');
 
   var response = {
