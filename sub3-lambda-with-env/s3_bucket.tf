e60bc50f3a32068fb28e5a16a4958bcf55d9c552
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-06-28 10:14:59 +0300

    update

--- sub3-lambda-with-env/s3_bucket.tf
+++ sub3-lambda-with-env/s3_bucket.tf
@@ -1,10 +1,6 @@
 resource "aws_s3_bucket" "sprintray-tf-state-files" {
   bucket = "sprintray-tf-state-files"
 
-  versioning {
-    enabled = true
-  }
-
     lifecycle {
       prevent_destroy = false # should be true to prevent accidentally deleting state file
     }
