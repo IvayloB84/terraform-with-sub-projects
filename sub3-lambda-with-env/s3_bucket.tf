eb5894db7b9bf6faba558e83487e50e7cd8dea1e
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-06-28 09:59:51 +0300

    update

--- sub3-lambda-with-env/s3_bucket.tf
+++ sub3-lambda-with-env/s3_bucket.tf
@@ -1,28 +1,11 @@
-data "aws_s3_bucket" "bucket" {
+resource "aws_s3_bucket" "sprintray-tf-state-files" {
   bucket = "sprintray-tf-state-files"
-}
 
-output "sprintray-tf-state-files" {
-  value = "${data.aws_s3_bucket.bucket.bucket}"
-}
+  versioning {
+    enabled = true
+  }
 
-resource "aws_s3_bucket" "bucket" {
-    count = var.create ? 1 : 0 
-    bucket = "sprintray-tf-state-files"
-    versioning {
-        enabled = true
-    }
-    server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-                sse_algorithm = "AES256"
-            }
-        }
-    }
-    object_lock_configuration {
-        object_lock_enabled = "Enabled"
-    }
-    tags = {
-        Name = "S3 Remote Terraform State Store"
-    }
+  lifecycle {
+    prevent_destroy = true
+  }
 }
