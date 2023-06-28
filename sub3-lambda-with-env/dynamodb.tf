fb463c153de8f72f25486bd67053762bcedbf3c9
IvayloB84 <ivaylo.bumbovski@gmail.com>
2023-06-28 10:08:58 +0300

    update

--- sub3-lambda-with-env/dynamodb.tf
+++ sub3-lambda-with-env/dynamodb.tf
@@ -1,9 +1,11 @@
 resource "aws_dynamodb_table" "state_locking" {
   hash_key = "LockID"
   name     = "sprintray-tf-state-files"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  billing_mode = "PAY_PER_REQUEST"
+    billing_mode = "PAY_PER_REQUEST"
+    attribute {
+      name = "LockID"
+      type = "S"
+
+    }
+  
 }
--- sub3-lambda-with-env/s3_bucket.tf
+++ sub3-lambda-with-env/s3_bucket.tf
@@ -5,7 +5,17 @@ resource "aws_s3_bucket" "sprintray-tf-state-files" {
     enabled = true
   }
 
-  lifecycle {
-    prevent_destroy = true
-  }
+    lifecycle {
+      prevent_destroy = false # should be true to prevent accidentally deleting state file
+    }
+    versioning {
+      enabled = true
+    }
+    server_side_encryption_configuration {
+      rule{
+          apply_server_side_encryption_by_default{
+              sse_algorithm = "AES256"
+          }
+      }
+    }
 }
